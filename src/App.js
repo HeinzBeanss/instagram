81e3f73ca10e71a2ec1175a313864c53b0a75d65
David Bean <heinzbeansss@gmail.com>
2022-12-27 17:31:13 +0000

    Create basic folder structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import './CSS/App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Nav from "./Components/Nav";
+import Home from "./Components/Home"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter >
+    {/* ADD basename={process.env.PUBLIC_URL} to the BrowserRouter element when deploying! */}
+        <Nav />
+        
+        <Routes>
+            <Route path={"/"} element={<Home />} />
+            {/* <Route path={"/profile}"} element={ } /> */}
+            
+        </Routes>
+        
+    </BrowserRouter>
+    )
 }
 
 export default App;
